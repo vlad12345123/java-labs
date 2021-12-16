792279bee676653e2fcc6b36fa35a076c6241bfb
vlad12345123 <vzezjulin@gmail.com>
2021-12-16 18:46:57 +0300

    refactor

---
+++ xml/idz8/src/src/Main.java
@@ -0,0 +1,72 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    private static final String FILENAME = "123.aspx.xml";
+
+    public static void main(String[] args) {
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(FILENAME));
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Main elem: " + doc.getDocumentElement().getNodeName());
+
+            NodeList list = doc.getElementsByTagName("staff");
+
+            for (int temp = 0; temp < list.getLength(); temp++) {
+
+                Node node = list.item(temp);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element element = (Element) node;
+
+                    String id = element.getAttribute("id");
+
+                    //attr
+                    String firstname = element.getElementsByTagName("firstname").item(0).getTextContent();
+                    String lastname = element.getElementsByTagName("lastname").item(0).getTextContent();
+                    String nickname = element.getElementsByTagName("nickname").item(0).getTextContent();
+
+                    NodeList salaryNodeList = element.getElementsByTagName("salary");
+                    String salary = salaryNodeList.item(0).getTextContent();
+
+                    String currency = salaryNodeList.item(0).getAttributes().getNamedItem("currency").getTextContent();
+
+                    System.out.println("Current Element :" + node.getNodeName());
+                    System.out.println("Staff Id : " + id);
+                    System.out.println("First Name : " + firstname);
+                    System.out.println("Last Name : " + lastname);
+                    System.out.println("Nick Name : " + nickname);
+                    System.out.printf("Salary [Currency] : %f\n", Float.parseFloat(salary), currency);
+
+                }
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
